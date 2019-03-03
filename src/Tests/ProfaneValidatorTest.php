21574caa25e7029825f2fdd105cbc44cf73946dd
Dorian Neto <doriansampaioneto@gmail.com>
2019-03-03 16:35:18 -0300

    add phpunit

---
+++ src/Tests/ProfaneValidatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DorianNeto\SymfonyProfaneBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ProfaneValidatorTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
